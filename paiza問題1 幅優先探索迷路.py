15d96e7670d0f6d50b35117a2040703d2e87b2fb
masami-fukuodori <m.2153.end@gmail.com>
2021-07-23 15:35:53 +0900

    paiza問題1 幅優先探索迷路の分析

--- paiza問題1 幅優先探索迷路.py
+++ paiza問題1 幅優先探索迷路.py
@@ -1,3 +1,4 @@
+#問題1 幅優先探索迷路
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
@@ -15,13 +16,15 @@ class State:
 
 
 def bfs(h, w, t, sx, sy, gx, gy):
-    open_list = []#未到達リスト
+    open_list = []#未調査リスト
     closed = set()#調査済みリスト
-    initial_state = State(sx, sy)
-    open_list.append(initial_state)
+    initial_state = State(sx, sy)#初期位置指定
+    open_list.append(initial_state)#初期位置を未調査リストに入れる
 
+    #初期位置
     cost = 1
     while True:
+        #一時保管場所
         tmp_q = []
         while open_list:
             #現在位置から見て、まだ進んでない道を取得
@@ -29,7 +32,7 @@ def bfs(h, w, t, sx, sy, gx, gy):
 
             if st.x == gx and st.y == gy:
                 return cost
-            #壁は飛ばす処理
+            #壁を飛ばす処理
             if t[st.y][st.x] == 1:
                 continue
             #通ってきた場所は飛ばす処理➡同地性判定、ハッシュ値取得が必要？
@@ -41,12 +44,16 @@ def bfs(h, w, t, sx, sy, gx, gy):
                 nx = st.x + dx[i]
                 ny = st.y + dy[i]
 
+                #座標が外れるものを排除する処理
                 if not (0 <= nx <= w - 1 and 0 <= ny <= h - 1):
                     continue
 
+                #一時保管の処理
                 tmp_q.append(State(nx, ny))
 
+        #次の未調査リストに追加
         open_list = tmp_q
+        #現在位置からの調査が全て終わるとcostを追加
         cost += 1
 
 
