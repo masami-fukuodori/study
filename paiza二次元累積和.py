86d8401284e4582ae16dac05d5e99301d6d29def
masami-fukuodori <m.2153.end@gmail.com>
2021-11-08 12:08:32 +0900

    paiza解答　二次元累積和追加

--- paiza二次元累積和.py
+++ paiza二次元累積和.py
@@ -1,19 +1,21 @@
 h, w, n = map(int, input().split())
 
-line = []
+block = []
 
+#横列毎に累積和のリストを作成
 for i in range(h):
-    s = list(map(int, input().split()))
-    for j in range(1, len(s)):
-        
-        s[j] = s[j-1] + s[j]
-        
-    line.append(s)
+    line = [0]
+    a = list(map(int, input().split()))
+    for j in range(w):
+        line.append(line[j] + a[j])
+    block.append(line)
+#print(block)
 
-
+#y点までのx点を足していく
 for i in range(n):
     y, x = map(int, input().split())
     total = 0
-    for s in range(y):
-        total += line[s][x]-1
+    for i_y in range(y):
+        #print(block[i_y][x])
+        total += block[i_y][x]
     print(total)
